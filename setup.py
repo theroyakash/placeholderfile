20176b8ea986f5842e1101badd7e53b355f24ea1
theroyakash <business.theroyakash@outlook.com>
2020-11-01 05:21:48 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ __version__ = '1.1'
 
 here = path.abspath(path.dirname(__file__))
 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='placeholderfile',
     version=__version__,
     description='Generates placeholderfile name',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/theroyakash/placeholderfile',
     download_url='https://github.com/theroyakash/placeholderfile/tarball/main',
     license='Apache License',
